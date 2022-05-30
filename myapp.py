5bcec1c9cfa2b06ad5972e7c0e331341656a53bb
Carmel Shvartzman <carmel.aspnet@gmail.com>
2022-05-30 13:11:59 +0300

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.write("""
+# Current Stocks Price Quotes
+Showing the stock closing price and volume of : 
+""")
+
+st.write("""
+## Google Inc.
+""")
+
+
+
+
+
+st.write("""
+         [Powered by CarmelSoft](https://carmelsoft.netlify.app "CarmelSoft's Homepage")
+          """)
